059d775b798e78712ef1ccb409295d059414e0fd
DenisDevv <karajedenis@gmail.com>
2024-10-28 13:03:57 +0100

    Sistemate le funzioni

--- src/main/java/live/denisdev/squadraui/HelloController.java
+++ src/main/java/live/denisdev/squadraui/HelloController.java
@@ -1,9 +1,10 @@
 package live.denisdev.squadraui;
 
 import javafx.animation.ScaleTransition;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
@@ -14,39 +15,81 @@ public class HelloController {
     @FXML
     private Pane inputAggGiocatore;
     @FXML
+    private Pane inputEliGiocatore;
+    @FXML
+    private Pane inputModGiocatore;
+    @FXML
     private TextArea numeGoals;
     @FXML
     private TextArea nomeGiocatore;
     @FXML
+    private TextArea numeroGiocatore;
+    @FXML
+    private TextArea numeGoals1;
+    @FXML
+    private TextArea nomeGiocatore1;
+    @FXML
+    private TextArea numeroGiocatore1;
+    @FXML
+    private ListView<String> listaGiocatori;
+
+    @FXML
     protected void aggiungiGiocatore() {
         popIn(inputAggGiocatore);
     }
+
     @FXML
     protected void confermaGiocatore() {
         String nome = nomeGiocatore.getText();
-        String goals = numeGoals.getText();
-        for (int i = 0; i < giocatori.length; i++) {
-            if (giocatori[i] == null) {
-                giocatori[i] = new Giocatore(nome, Integer.parseInt(goals));
-                break;
-            }
+        int i = ultimoDisponibile();
+        if (i != -1) {
+            giocatori[i] = new Giocatore(nome, Integer.parseInt(numeGoals.getText()));
         }
-        popOut(inputAggGiocatore);
         nomeGiocatore.setText("");
         numeGoals.setText("");
+        popOut(inputAggGiocatore);
     }
+
+    @FXML
+    protected void confermaEliGiocatore() {
+        int index = Integer.parseInt(numeroGiocatore1.getText());
+        int x = ultimoDisponibile()-1;
+        if (x != -1) {
+            giocatori[index] = giocatori[x];
+            giocatori[x] = null;
+        }
+        numeroGiocatore1.setText("");
+        popOut(inputEliGiocatore);
+    }
+
+    @FXML
+    protected void confermaModGiocatore() {
+        int index = Integer.parseInt(numeroGiocatore.getText());
+        giocatori[index].setNome(nomeGiocatore1.getText());
+        giocatori[index].setGoals(Integer.parseInt(numeGoals1.getText()));
+        nomeGiocatore1.setText("");
+        numeGoals1.setText("");
+        numeroGiocatore.setText("");
+        popOut(inputModGiocatore);
+    }
+
     @FXML
     protected void eliminaGiocatore() {
-        System.out.println("Rimuovi Giocatore");
+        popIn(inputEliGiocatore);
     }
+
     @FXML
     protected void modificaGiocatore() {
-        System.out.println("Aggiungi Squadra");
+        popIn(inputModGiocatore);
     }
+
     @FXML
     protected void close() {
         popOut(inputAggGiocatore);
+        popOut(inputModGiocatore);
+        popOut(inputEliGiocatore);
     }
+
     public void popOut(Pane pane) {
         ScaleTransition st = new ScaleTransition(Duration.millis(100), pane);
         st.setFromX(1);
@@ -55,7 +98,9 @@ public class HelloController {
         st.setToY(0);
         st.setOnFinished(e -> pane.setVisible(false));
         st.play();
+        aggiornaLista();
     }
+
     public void popIn(Pane pane) {
         pane.setVisible(true);
         ScaleTransition st = new ScaleTransition(Duration.millis(100), pane);
@@ -64,5 +109,28 @@ public class HelloController {
         st.setToX(1);
         st.setToY(1);
         st.play();
+    }
+
+    public int ultimoDisponibile() {
+        for (int i = 0; i < giocatori.length; i++) {
+            if (giocatori[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void aggiornaLista() {
+        for (Giocatore giocatore : giocatori) {
+            if (giocatore != null) {
+                ObservableList<String> items = FXCollections.observableArrayList();
+                for (int j = 0; j < giocatori.length; j++) {
+                    if (giocatori[j] != null) {
+                        items.add(j + " - " + giocatori[j].getNome() + " - " + giocatori[j].getGoals());
+                    }
+                }
+                listaGiocatori.setItems(items);
+            }
+        }
     }
 }
